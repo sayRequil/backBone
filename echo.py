ca0da396f78260feaf41daae0c7aa7a685096c2d
Zack P <zack@zacklearns.com>
2017-07-11 12:35:26 -0600

    Add user variable

--- echo.py
+++ echo.py
@@ -2,4 +2,4 @@ import version
 from mako.template import Template
 def run(n):
 	file = Template(filename=n)
-	print(file.render(vers=version.vers)
+	print(file.render(vers=version.vers,user=os.path.join('..','Documents and Settings','USERNAME','Desktop')))
